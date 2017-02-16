3dce40a2d93bd456a7d7543e60bea15d7ec9606d
ekivolowitz <ekivolowitz@wisc.edu>
2017-02-15 23:25:21 -0600

    Updating .gitignore to ignore output text files.

--- GetData.py
+++ GetData.py
@@ -5,14 +5,30 @@ import os
 import time
 import sys 
 
-CKEY = ''
-CSECRET = ''
-AKEY = ''
-ASECRET = ''
-
 
 client = MongoClient()
 db = client.IDs
+
+AUTH_KEYS = []
+
+def authenticate(listWithAuthKeys):
+	auth = tweepy.OAuthHandler(listWithAuthKeys[0], listWithAuthKeys[1])
+	auth.set_access_token(listWithAuthKeys[2], listWithAuthKeys[3])
+	AUTH_KEYS.append(tweepy.API(auth))
+
+def initAuthKeys(keyCount):
+	try:
+		with open('secrets.json', 'r') as f:
+			data = json.load(f)
+			CKEY = data["auth" + str(keyCount)]["CKEY"]
+			CSECRET = data["auth" + str(keyCount)]["CSecret"]
+			AKEY = data["auth" + str(keyCount)]["AToken"]
+			ASECRET = data["auth" + str(keyCount)]["ASecret"]
+			key = [CKEY, CSECRET, AKEY, ASECRET]
+			authenticate(key)
+			initAuthKeys(keyCount + 1)
+	except KeyError:
+		print("Used all keys.")
 
 
 def formatJson(id, followersList, following):
@@ -27,7 +43,7 @@ def formatJson(id, followersList, following):
 	return user
 
 def getUserFollowers(follower, api):
-	
+	print("Getting info for " + str(follower))
 	followerIDS = []
 	followingIDS = []
 	dbCollectionName = "info_" + str(follower) 
@@ -44,33 +60,41 @@ if __name__ == '__main__':
 	
 	lookupTerm = sys.argv[1]
 	dbCollectionName = "info_" + lookupTerm
-
-	# with open('config.json', 'r') as f:
-	#     data = json.load(f)
-	#     CKEY = data["auth1"]["CKEY"]
-	#     CSECRET = data["auth1"]["CSecret"]
-	#     AKEY = data["auth1"]["AToken"]
-	#     ASECRET = data["auth1"]["ASecret"]
-
+	initAuthKeys(0)
+	#Get initial following/follower list of user entered. 
+	followerIDS = []
+	followingIDS = []
+	CKEY = ""
+	CSECRET = ""
+	AKEY = ""
+	ASECRET = ""
+	keyCount = 0 
+	with open('secrets.json', 'r') as f:
+		data = json.load(f)
+		CKEY = data["auth" + str(keyCount)]["CKEY"]
+		CSECRET = data["auth" + str(keyCount)]["CSecret"]
+		AKEY = data["auth" + str(keyCount)]["AToken"]
+		ASECRET = data["auth" + str(keyCount)]["ASecret"]
+	#api = AUTH_KEYS[0]
+	print("CKEY: " + str(CKEY) + "\n" + "CSECRET: " + str(CSECRET) + "\nAKEY: " + str(AKEY) + "\nASECRET" + str(ASECRET))
 	auth = tweepy.OAuthHandler(CKEY, CSECRET)
 	auth.set_access_token(AKEY, ASECRET)
 	api = tweepy.API(auth)
 	
-
-	#Get initial following/follower list of user entered. 
-	followerIDS = []
-	followingIDS = []
-	for page in tweepy.Cursor(api.followers_ids, id=lookupTerm).pages(1):
+		
+	print("Line 68")
+	for page in tweepy.Cursor(AUTH_KEYS[0].followers_ids, id=lookupTerm).pages(1):
+		print("Getting followers for " + str(lookupTerm))
 		followerIDS.extend(page)
 		time.sleep(61)
-
-	for page in tweepy.Cursor(api.friends_ids, id=lookupTerm).pages(1):
+	
+	print("line 74")
+	for page in tweepy.Cursor(AUTH_KEYS[0].friends_ids, id=lookupTerm).pages(1):
+		print("Getting people who " + str(lookupTerm) + " follows")
 		followingIDS.extend(page)
 		time.sleep(61)
 	
-		
+	print("Line 80")
 	db[dbCollectionName].insert_one(formatJson(lookupTerm, followerIDS, followingIDS))
-
 	for follower in followerIDS:
-		getUserFollowers(follower, api)
-
+		getUserFollowers(follower, AUTH_KEYS[0])	
